865f572753fa69ee722f45b0fbc05d85bed1d0a6
Manuel Martín <manuelmartin@Mac-mini-de-Manuel.local>
2025-05-19 09:58:18 +0200

    Herencia Multiple, nuevo ejemplo

--- 6_Orientacion_a_objetos/6.9-herencia_multiple.py
+++ 6_Orientacion_a_objetos/6.9-herencia_multiple.py
@@ -35,3 +35,49 @@ c.saludar()
 # Sin embargo, es importante usarla con precaución y asegurarse de que el diseño de la clase sea claro y fácil de entender.
 
 
+class Animal:
+    @property
+    def terrestre(self):
+        return True
+    
+class Felino (Animal):
+    @property
+    def garras_retractiles(self):
+        return True
+    
+    def cazar(self):
+        print ("El felino esta Cazando")
+
+class Mascota:
+    nombre = " " #Todas las mascotas tienen un nombre
+    
+    @property
+    def domestico(self):
+        return True
+    
+    def jugar(self):
+        print ("La mascota esta jugando")
+    
+    def mostrar_nombre(self):
+        print (f"El nombre de la mascota es: {self.nombre}")
+
+# La clase Gato hereda de las clases Felino y Mascota
+class Gato (Felino, Mascota):
+    @property
+    def domestico(self):
+        return True
+    
+    def maullar(self):
+        print ("El gato esta maullando")
+# En este ejemplo, la clase Gato hereda de las clases Felino y Mascota.
+# La clase Gato tiene acceso a los métodos y propiedades de ambas clases.
+# La clase Gato tiene su propio método maullar, pero también puede usar los métodos cazar y jugar de las clases Felino y Mascota.
+# La clase Gato también tiene su propia propiedad domestico, que sobrescribe la propiedad domestico de la clase Mascota.
+# La clase Gato tiene acceso a la propiedad terrestre de la clase Animal a través de la herencia de la clase Felino.
+# La clase Gato hereda de las clases Felino y Mascota, lo que significa que la clase Gato es una subclase de ambas clases.
+
+        
+gato  = Gato()
+gato.nombre = "Michi"
+gato.mostrar_nombre()
+print (f"Mostramos la propiedad de la clase Animal del gato es 'terrestre' valor: {gato.terrestre}")
